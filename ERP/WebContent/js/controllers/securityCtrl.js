4e2c8aa1dc7ed747956e8ed22c3902976630ebd7
nikhil raskar <nikhilraskar50@gmail.com>
2017-02-09 01:25:08 -0800

    To do for data send
    Signed-off-by: nikhil raskar <nikhilraskar50@gmail.com>

--- ERP/WebContent/js/controllers/securityCtrl.js
+++ ERP/WebContent/js/controllers/securityCtrl.js
@@ -1,16 +1,14 @@
-erpApp.controller('securityCtrl', function($scope, $http, $mdDialog, $mdToast, $rootScope,
-		SERVER_URL) {
-          
-	
-	$scope.getRMInformation=function()
-	
+erpApp.controller('securityCtrl', function($scope, $http, $mdDialog, $mdToast,
+		$rootScope, SERVER_URL) {
+
+	$scope.getRMInformation = function()
+
 	{
 		$http({
 			method : 'GET',
 			url : SERVER_URL + "rawmaterialorder/list"
 		}).then(function successCallback(response) {
-			$scope.rawMaterials = response.data;
-			
+			$scope.rawMaterialOrders = response.data;
 
 			console.log(response);
 
@@ -19,15 +17,13 @@ erpApp.controller('securityCtrl', function($scope, $http, $mdDialog, $mdToast, $
 
 		});
 	}
-	
-	$scope.displayRMList=function(index)
-	{
+
+	$scope.displayRMList = function(index) {
 		console.log($scope.rawMaterials)
 		$http({
-			method : 'GET',
-			url : SERVER_URL + "rawmaterial/getRMForRMOrder/"+ $scope.rawmaterialorderassociation.id
-		
-		}).then(function successCallback(response) {
+				method : 'GET',
+				url : SERVER_URL + "rawmaterial/getRMForRMOrder/"+ $scope.rawMaterialOrder
+				}).then(function successCallback(response) {
 			$scope.rawMaterialList = response.data;
 
 			console.log(response);
@@ -37,103 +33,65 @@ erpApp.controller('securityCtrl', function($scope, $http, $mdDialog, $mdToast, $
 
 		});
 	}
-	
+
 	$scope.submitInformation = function(isvaliduser, $event) {
-		if (isvaliduser) {
+//		if (isvaliduser) {
 			$scope.saveSecurityInformation();
-		} else {
-			console.log('its else block');
+//		} else {
+//			console.log('its else block');
+//		}
+	};
+	$scope.checkErr = function(intime, outtime) {
+		$scope.errMessage = '';
+		/* var curTime = new Time(); */
+		$scope.curTime = new Time();
+
+		if (new Time(intime) > new Time(outtime)) {
+			$scope.errMessage = 'End Date should be greater than start date';
+			return false;
+		}
+		if (new Date(intime) < curTime) {
+			$scope.errMessage = 'Start date should not be before today.';
+			return false;
 		}
 	};
-	/* $scope.checkErr=function(intime,outtime)
-	 {
-		 $scope.errMessage = '';
-	        var curTime = new Time();
-		 $scope.curTime=new Time();
-	        
-	        if(new Time(intime) > new Time(outtime)){
-	          $scope.errMessage = 'End Date should be greater than start date';
-	          return false;
-	        }
-	        if(new Time(intime) < curTime){
-	           $scope.errMessage = 'Start date should not be before today.';
-	           return false;
-	        }
-	 };*/
-	
-	$scope.saveSecurityInformation=function()
-	{
+
+	$scope.saveSecurityInformation = function() {
 		console.log('its save function')
+		
+		// iterate populated Raw material table and create list of objects with 'id' and 'quantity' for each row.
+		//assign that list to 'rawmaterialorderinvoiceassociations' in data
 		var data = {
 
-				invoice_No: $scope.invoice_No,
-				vendorname:$scope.vendorname,
-				vehicleNo:$scope.vehicleNo,
-				driver_Name:$scope.driver_Name,
-				description:$scope.description,
-				createDate:2017-01-25,
-				/*intime:'01:30:20',
-				outtime:'01:30:20',*/
-				intime:$scope.intime,
-				outtime:$scope.outtime,
-				status:4,
-				po_No: $scope.po_No,
-				createdBy:2,
-				created_date:null,
-				updatedBy:1,
-				updated_date:null,
-				isactive:true
-			};
+			invoice_No : $scope.invoice_No,
+			vendorname : $scope.vendorname,
+			vehicleNo : $scope.vehicleNo,
+			driver_Name : $scope.driver_Name,
+			description : $scope.description,
+			createDate : 2017 - 01 - 25,
+			intime : null,
+			outtime : null,
+			status : 4,
+			po_No : $scope.po_No,
+			createdBy : 2,
+			created_date : null,
+			updatedBy : 1,
+			updated_date : null,
+			isactive : true,
+			rawmaterialorderinvoiceassociations : $scope.rawMaterialList
+		};
 		$http({
 			method : 'post',
 			url : SERVER_URL + "rawmaterialorderinvoice/securitycheck",
 			data : data
 		}).then(function successCallback(data) {
-			if (data.code === 0) {
-				console.log(data.message);
-				console.log(data);
-				$scope.hide();
-				$scope.message = 'Something went worng. Please try again later.';
-				$scope.showToast();
-			} 
-			else {
-				$scope.displayProgressBar = false;
-				$scope.message = 'Information saved successfully.';
-				$scope.showToast();
-			}
-			
-			
+
+			console.log(data);
 		}, function errorCallback(response) {
 			console.log("Error");
 
 		});
-		
-	}
-	
-	$scope.showToast = function() {
-		$mdToast.show({
-			hideDelay : 3000,
-			position : 'top right',
-			controller : 'ToastCtrl',
-			templateUrl : 'views/toast.html',
-			locals : {
-				message : $scope.message
-			}
-		});
-	};
-	function ProgressBarController($scope, $mdDialog) {
-		
-		$scope.hide = function() {
-			$mdDialog.hide();
-		};
 
-		$scope.cancel = function() {
-			$mdDialog.cancel();
-		};
-
-		$scope.answer = function(answer) {
-			$mdDialog.hide(answer);
-		};
 	}
-	
+
 });
