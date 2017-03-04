b0a83fbf86e80e18ad25ccd64dbdbb519d802228
NishantNaikwade <nishant.naikwade@gmail.com>
2017-03-04 09:11:23 +0530

    Generic ws service

---
+++ ERP/WebContent/js/services/erpWSService.js
@@ -0,0 +1,40 @@
+erpApp.service('erpWSService',function erpWSService(Auth,SERVER_URL){
+	var USER_LIST = 'user/list';
+	
+	this.getHTTPParams = function (method, url, data, headers){
+		var requestBody = {};
+		
+		//create your request body here and return object
+		requestBody.method = method;
+		requestBody.url = SERVER_URL + url;
+		requestBody.headers = this.getHTTPRequestHeader();
+		requestBody.data = this.getWSData();
+		return requestBody;
+	};
+	this.getHTTPRequestBody = function(){
+		var data = {};
+		
+		return data;
+	};
+	this.getHTTPRequestHeader = function (){
+		var headers = {
+				auth_token : Auth.getAuthToken()
+		};
+		return headers;
+	};
+	
+	this.getList = function(){
+		
+		var httpparams = this.getHTTPParams('GET',USER_LIST);
+		
+		$http(httpparams).then(function successCallback(data) {
+				$mdDialog.hide();
+				$rootScope.$emit("CallPopulateUserList", {});
+				console.log(data);
+				}, function errorCallback(data) {
+					console.log("Error");
+		});
+	};
+	
+	
+});
