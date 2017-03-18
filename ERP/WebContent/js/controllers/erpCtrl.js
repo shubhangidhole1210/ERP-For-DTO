ec1068316e27150b30acd58821c620ed303daacd
shubhangi dhole <shubhangidhole12@gmail.com>
2017-03-18 13:02:13 +0530

    code changes

--- ERP/WebContent/js/controllers/erpCtrl.js
+++ ERP/WebContent/js/controllers/erpCtrl.js
@@ -1,65 +1,22 @@
 erpApp.controller('ERPController', function($scope,$rootScope,Auth,SERVER_URL,$http,$location,User) {
 	
-	/*$scope.menu = [];
-	$scope.displayMenu=Auth.isLoggedIn();*/
-	/*$scope.displayUserName = Auth.isLoggedIn();*/
-	/*if($scope.displayMenu){
-		$scope.menu = Auth.getMenu();
-	}*/
 	$rootScope.$on('logout',function($event){
 		console.log('Inside logout event');
-//		$scope.displayMenu=Auth.isLoggedIn();
-		/*$scope.displayUserName = Auth.isLoggedIn();*/
 		$scope.displayLoginButton = Auth.isLoggedIn();
-		/*$scope.displayUserName=Auth.isLoggedIn();*/
 		console.log('logging out');
 		$location.path('/login');
 
 	});
 	$rootScope.$on('loginSuccess',function($event){
 		console.log('Inside login success event');
-//		$scope.displayMenu=Auth.isLoggedIn();
-		/*$scope.displayUserName = Auth.isLoggedIn();*/
-		/*$scope.displayUserName=Auth.isLoggedIn();*/
-//		$scope.menu = Auth.getMenu();
 		$scope.user = User;
-		
-	});
-	
-	$scope.logginButton=[];
-	$scope.displayLoginButton=Auth.isLoggedIn();
-	if($scope.displayLoginButton){
-		$scope.logginButton = Auth.getMenu();
-	
-	}
-	$rootScope.$on('loginSuccess',function($event){
-		console.log('Inside login success event');
+		$scope.displayUserName=Auth.isLoggedIn();
+		$scope.userId = Auth.getMenu();
 		$scope.displayLoginButton=Auth.isLoggedIn();
 		$scope.logginButton = Auth.getMenu();
 		
-	});
-	
-	$scope.userId=[];
-	$scope.displayUserName=Auth.isLoggedIn();
-	if($scope.displayUserName){
-		$scope.userId=Auth.getMenu();
-	}
-		
-	$rootScope.$on('loginSuccess',function($event){
-		console.log('Inside login success event');
-		$scope.displayUserName=Auth.isLoggedIn();
-		$scope.userId = Auth.getMenu();
 		
 	});
-	
-	
-	
-	
-	/*$scope.userID=[];
-	$scope.isUserName=Auth.isLoggedIn();
-	if($scope.isUserName){
-		$scope.userID = Auth.getMenu();
-	}*/
 	
 	$rootScope.$on("CallUserProfileList", function($event,$index) {
 		$scope.getUserProfile();
--- ERP/WebContent/js/controllers/qualityInspectionCtrl.js
+++ ERP/WebContent/js/controllers/qualityInspectionCtrl.js
@@ -1,151 +1,130 @@
-erpApp.controller('qualityInspectionCtrl', function($scope, $http, $mdDialog, $mdToast, $rootScope,
-		SERVER_URL,utils,Auth,$location) {
-$scope.getRMOrderInvoiceInformation=function()
-	
-	{
-	var httpparams = {};
-	httpparams.method = 'GET';
-	httpparams.url = SERVER_URL + "rawmaterialorderinvoice/security-in-invoices";
-	httpparams.headers = {
-			auth_token : Auth.getAuthToken()
-		};
-	$http(httpparams).then(function successCallback(response) {
-			$scope.invoiceList = response.data;
-			console.log(response);
-			$mdDialog.hide();
-		}, function errorCallback(response) {
-			console.log("Error");
-			$mdDialog.hide();
-		});
-		 $scope.showProgressBarOne();
-	}
+erpApp
+		.controller(
+				'qualityInspectionCtrl',
+				function($scope, $http, $mdDialog, $mdToast, $rootScope,
+						SERVER_URL, utils, Auth, $location) {
+					$scope.getRMOrderInvoiceInformation = function()
 
-$scope.invoiceRawMaterialList=function(index)
-{
-	var httpparams = {};
-	httpparams.method = 'GET';
-	httpparams.url = SERVER_URL + "qualitycheckrawmaterial/listrm/" + $scope.invoiceList.id;
-	httpparams.headers = {
-			auth_token : Auth.getAuthToken()
-		};
-	$http(httpparams).then(function successCallback(response) {
-		$scope.rmInvoiceList = response.data;
-		console.log($scope.rmInvoiceList)
-		console.log(response);
-		$mdDialog.hide();
-	}, function errorCallback(response) {
-		console.log("Error");
-		$mdDialog.hide();
-	});
-	 $scope.showProgressBarOne();
-	};
+					{
+						var httpparams = {};
+						httpparams.method = 'GET';
+						httpparams.url = SERVER_URL
+								+ "rawmaterialorderinvoice/security-in-invoices";
+						httpparams.headers = {
+							auth_token : Auth.getAuthToken()
+						};
+						$http(httpparams).then(
+								function successCallback(response) {
+									$scope.invoiceList = response.data;
+									console.log(response);
+									utils.hideProgressBar();
+								}, function errorCallback(response) {
+									console.log("Error");
+									utils.hideProgressBar();
+								});
+						utils.showProgressBar();
+					};
 
-   $scope.submitInformation=function(isvaliduser, $event)
-   {
-	   if (isvaliduser) {
-			$scope.saveQualityinspectionInformation();
-		} else {
-			console.log('its else block');
-	}
-   };
-   
-   $scope.resetQualityInspection=function()
-   {
-	   $scope.user= angular.copy(original);
-		$scope.qualityInspectionForm.$setPristine();
-   }
-   $scope.showProgressBarOne= function()
-	{
-		$mdDialog
-		.show(
-				{
-					controller : ProgressBarController,
-					templateUrl : 'views/progressBar.html',
-					parent : angular
-							.element(document.body),
-					clickOutsideToClose : false,
-					fullscreen : $scope.customFullscreen,
-					onComplete : function() {
-					}
-				})
-		.then(
-				function(answer) {
-					$scope.status = 'You said the information was "'
-							+ answer + '".';
-				},
-				function() {
-					$scope.status = 'You cancelled the dialog.';
+					$scope.invoiceRawMaterialList = function(index) {
+						var httpparams = {};
+						httpparams.method = 'GET';
+						httpparams.url = SERVER_URL
+								+ "qualitycheckrawmaterial/listrm/"
+								+ $scope.invoiceList.id;
+						httpparams.headers = {
+							auth_token : Auth.getAuthToken()
+						};
+						$http(httpparams).then(
+								function successCallback(response) {
+									$scope.rmInvoiceList = response.data;
+									console.log($scope.rmInvoiceList)
+									console.log(response);
+									utils.hideProgressBar();
+								}, function errorCallback(response) {
+									console.log("Error");
+									utils.hideProgressBar();
+								});
+						utils.showProgressBar();
+					};
+
+					$scope.submitInformation = function(isvaliduser, $event) {
+						if (isvaliduser) {
+							$scope.saveQualityinspectionInformation();
+						} else {
+							console.log('its else block');
+						}
+					};
+
+					$scope.resetQualityInspection = function() {
+						$scope.user = angular.copy(original);
+						$scope.qualityInspectionForm.$setPristine();
+					};
+
+					$scope.saveQualityinspectionInformation = function() {
+						console.log('its save function');
+						var index = 0;
+						for (index = 0; index < $scope.rmInvoiceList.length; index++) {
+							$scope.rmInvoiceList[index].intakeQuantity = $scope.rmInvoiceList[index].quantity;
+						}
+						var data = {
+
+							description : $scope.description,
+							id : $scope.invoiceList.id,
+							qualitycheckrawmaterials : $scope.rmInvoiceList
+
+						};
+
+						var httpparams = {};
+						httpparams.method = 'post';
+						httpparams.url = SERVER_URL
+								+ "qualitycheckrawmaterial/qualitycheck";
+						httpparams.headers = {
+							auth_token : Auth.getAuthToken()
+						};
+						httpparams.data = data;
+						$http(httpparams)
+								.then(
+										function successCallback(data) {
+											if (data.data.code === 1) {
+												console.log(data.data.message);
+												console.log(data);
+												$location.path('/')
+												utils
+														.showToast('Qualitycheckrawmaterial added Successfully !')
+												utils.hideProgressBar();
+											} else {
+												console.log(data);
+												utils
+														.showToast('something went wrong please try again')
+												utils.hideProgressBar();
+											}
+
+										},
+										function errorCallback(response) {
+											console.log("Error");
+											utils.hideProgressBar();
+											utils
+													.showToast('something went wrong please try again')
+										});
+						utils.showProgressBar();
+					};
+					$scope.ischeckBoxDisabled = true;
+
+					$scope.checkReceivedQuantity = function(index) {
+
+						console.log('checkReceivedQuantity');
+						if ($scope.rmInvoiceList[index].quantity === parseInt($scope.rmInvoiceList[index].goodQuantity)) {
+							$scope.rmInvoiceList[index].isReturnInvoiceInitated = false;
+							$scope.rmInvoiceList[index].ischeckBoxDisabled = true;
+						} else if ($scope.rmInvoiceList[index].quantity <= parseInt($scope.rmInvoiceList[index].goodQuantity)) {
+							$scope.rmInvoiceList[index].isReturnInvoiceInitated = false;
+							$scope.rmInvoiceList[index].ischeckBoxDisabled = true;
+						}else {
+							$scope.rmInvoiceList[index].isReturnInvoiceInitated = true;
+							$scope.rmInvoiceList[index].ischeckBoxDisabled = false;
+
+						}
+
+					};
+
 				});
-	};
-   $scope.saveQualityinspectionInformation=function()
-   {
-	   console.log('its save function');
-	   var index = 0;
-	   for(index = 0; index < $scope.rmInvoiceList.length; index++){
-		   $scope.rmInvoiceList[index].intakeQuantity = $scope.rmInvoiceList[index].quantity;
-	   }
-	   var data = {
-
-			   description:$scope.description,
-			   id: $scope.invoiceList.id,
-			   qualitycheckrawmaterials:$scope.rmInvoiceList
-			   
-			};
-	   
-	   var httpparams = {};
-		httpparams.method = 'post';
-		httpparams.url = SERVER_URL + "qualitycheckrawmaterial/qualitycheck";
-		httpparams.headers = {
-				auth_token : Auth.getAuthToken()
-			};
-		httpparams.data = data;
-		$http(httpparams).then(function successCallback(data) {
-			if(data.data.code === 1){
-				console.log(data.data.message);
-				console.log(data);
-				$location.path('/')
-				$scope.message = 'Qualitycheckrawmaterial added Successfully !';
-				$scope.showToast();
-				$mdDialog.hide();
-			}
-			else
-				{
-				console.log(data);
-				$scope.message = 'something went wrong please try again';
-				$scope.showToast();
-				$mdDialog.hide();
-				}
-			
-		}, function errorCallback(response) {
-			console.log("Error");
-			$mdDialog.hide();
-			$scope.message = 'Something went worng. Please try again later.';
-			$scope.showToast();
-		});
-	   $scope.showProgressBarOne();
-   }
-   $scope.showToast = function() {
-		$mdToast.show({
-			hideDelay : 3000,
-			position : 'top right',
-			controller : 'ToastCtrl',
-			templateUrl : 'views/toast.html',
-			locals : {
-				message : $scope.message
-			}
-		});
-	};
-   function ProgressBarController($scope, $mdDialog) {
-		
-		$scope.hide = function() {
-			$mdDialog.hide();
-		};
-		$scope.cancel = function() {
-			$mdDialog.cancel();
-		};
-		$scope.answer = function(answer) {
-			$mdDialog.hide(answer);
-		};
-	}
-
-});
