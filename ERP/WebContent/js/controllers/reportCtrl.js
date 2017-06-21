4abdd0289086876fa37b4e7781f956e8b9eb5885
NishantNaikwade <nishant.naikwade@gmail.com>
2017-06-21 09:29:00 +0530

    Report Response Integration Changes

--- ERP/WebContent/js/controllers/reportCtrl.js
+++ ERP/WebContent/js/controllers/reportCtrl.js
@@ -1,11 +1,6 @@
 erpApp.controller('reportCtrl', function($scope,$http, $mdDialog,SERVER_URL,$rootScope,$mdToast,Auth,utils) {
      $scope.isPagePresent=false;
-     $scope.page={};
-     
-    
-     
-	
-	$scope.setreports = function(){
+     $scope.setreports = function(){
 		$scope.reports = Auth.getReport();
 	}
 	
@@ -19,21 +14,6 @@ erpApp.controller('reportCtrl', function($scope,$http, $mdDialog,SERVER_URL,$roo
 			};
 		$http(httpparams).then(function successCallback(response) {
 			$scope.data = response.data;
-			var input = {};
-			input.reportinputparameter = {};
-			input.reportinputparameter.displayName = "Employee"
-			input.reportinputparameter.inputType = 'select';
-			input.reportinputparameter.data = [
-			      {
-			    	  id : "10",
-			    	  description: "Nishant"
-			      },
-			      {
-			    	  id : "11",
-			    	  description: "Nikhil"
-			      }
-			];
-			$scope.data.push(input);
 			console.log(response);
 		}, function errorCallback(response) {
 			console.log("Error");
